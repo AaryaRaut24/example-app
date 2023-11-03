546d1415b416477143a945a571e9ba6b443cd117
AaryaRaut24 <rautaarya24@gmail.com>
2023-11-03 21:40:28 +0530

    Update factoriald program for readability and maintainability

--- practicals/factorial.php
+++ practicals/factorial.php
@@ -1,14 +1,19 @@
 <?php
 function factorial($n) {
-    if ($n === 0) {
+    if ($n < 0) {
+        // Handle negative input as an error
+        return "Factorial is not defined for negative numbers";
+    } elseif ($n == 0 || $n == 1) {
+        // Base case: 0! and 1! are both 1
         return 1;
     } else {
+        // Recursive case: Multiply $n by factorial of ($n-1)
         return $n * factorial($n - 1);
     }
 }
 
-$number = 5; // Change this to the number for which you want to calculate the factorial
+// Example usage:
+$number = 5;
 $result = factorial($number);
-
-echo "The factorial of $number is $result";
+echo "Factorial of $number is $result";
 ?>
